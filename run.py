208ec8efda378b4800154395e5eb06f8d91d33f9
Hoyasumii <alanreisanjo@gmail.com>
2024-08-22 20:59:44 -0300

    Hehe

---
+++ run.py
@@ -0,0 +1,13 @@
+import sys, os
+
+
+with open(sys.argv[1], "r", encoding="utf-8") as doc:
+  data = [list(item) for item in [ item for item in doc.readlines() ]]
+
+  for item in data:
+    with (open("README.md", "a", encoding="utf=8")) as testFile:
+      for letter in item:
+        testFile.write(letter)
+        os.system("git add .")
+        os.system(f'git commit -m "A Letra \"{letter}\"" foi adicionada"')
+        os.system("git push")
