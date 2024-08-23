d10e99b985a3586e1d71cb3e6182756a3e8b309b
Hoyasumii <alanreisanjo@gmail.com>
2024-08-22 21:03:52 -0300

    Teste

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ with open(sys.argv[1], "r", encoding="utf-8") as doc:
   data = [list(item) for item in [ item for item in doc.readlines() ]]
 
   for item in data:
-    with (open("README.md", "a", encoding="utf=8")) as testFile:
-      for letter in item:
-        testFile.write(letter)
-        testFile.close()
-        os.system("git add .")
-        os.system(f'git commit -m "A Letra {letter} foi adicionada"')
-        os.system("git push")
+    for letter in item:
+      testFile = open("README.md", "a", encoding="utf=8")
+      testFile.write(letter)
+      testFile.close()
+      os.system("git add .")
+      os.system(f'git commit -m "A Letra {letter} foi adicionada"')
+      os.system("git push")
